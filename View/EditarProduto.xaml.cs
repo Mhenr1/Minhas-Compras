960c8617325491e98e8a38c68c7315afb556a39f
Mhenr1 <mhfozorio@gmail.com>
2026-03-30 08:31:52 -0300

    Agenda 5

--- View/EditarProduto.xaml.cs
+++ View/EditarProduto.xaml.cs
@@ -1,9 +1,37 @@
+using Minhas_Compras.Models;
+
 namespace Minhas_Compras.View;
 
 public partial class EditarProduto : ContentPage
 {
-	public EditarProduto()
-	{
-		InitializeComponent();
-	}
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked_Editar(object sender, EventArgs e)
+    {
+        try
+        {
+
+            Produto produto_contexto = BindingContext as Produto;
+            Produto p = new Produto()
+            {
+                
+                Descricao = txt_descricao.Text,
+                Preco = Convert.ToDouble(txt_preco.Text),
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Id = produto_contexto.Id,
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlertAsync("Sucesso", $"Produto {txt_descricao.Text} alterado com sucesso!", "OK");
+            Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlertAsync("Erro", "A alteração do produto falhou", "OK");
+
+        }
+    }
 }
